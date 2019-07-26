b1143deaf46a0068eaaed31fefbb4f8d166647a8
ariflateefshah <arif@ideas2it.com>
2019-07-26 16:42:24 +0530

    Feature: HTTP request using VueResource | GET  -- HTTP GET API request using vueResource

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <template>
-   <div class="form">
-        <form>
-          <p>
-            <label>Title :</label>
-            <input type="text" name="title" v-model.lazy="post.title">
-          </p>
-           <p>
-              <textarea name="body" v-model.lazy="post.body" placeholder="description"></textarea>
-          </p> 
-          <p>
-            <button v-on:click.prevent="blogPost">Publish</button>
-          </p>
-        </form>
-
-        <div class="data-preview">
+   
+     <div class="form">
+        <div class="data-preview" v-for="post in posts" :key="post.index" >
           <h3>{{post.title}}</h3>
           <p>{{post.body}}</p>
         </div>
-    </div>
+     </div>
 </template>
 
 <script>
@@ -26,28 +14,19 @@
 export default {
   data () {
     return {
-      post:{
-        userId:'1',
-        id:'1',
-        title:'',
-        body:''
-      }
+      posts:[],
     }
   },
   methods:{
-    blogPost(){
-      // Data section for Post Request
-       this.$http.post("https://jsonplaceholder.typicode.com/posts",{
-          title:this.post.title,
-          userId:1,
-          id:2,
-          body:this.post.body
-       }).then(function(data){
+    
+  }, 
+  beforeMount(){
+    this.$http.get("https://jsonplaceholder.typicode.com/posts").then(function(response){
         //  Response section
+        this.posts = response.data;
           console.log(data);
        });
-    }
-  },  
+  } 
 }
 </script>
 
@@ -60,5 +39,10 @@ export default {
   border-radius: 5px;
   box-shadow: 5px 5px 5px 4px;
   border:1px solid grey;
+}
+.data-preview{
+  margin: 2%;
+  border:1px solid grey;
+  padding: 10px;
 }
 </style>

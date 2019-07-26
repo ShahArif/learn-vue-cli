4ef8292dea011a6e72954df924e85c5d878485f0
ariflateefshah <arif@ideas2it.com>
2019-07-26 16:22:04 +0530

    Feature: HTTP Request By VueResource | POST  -- How make HTTP Post request  -- How to use fake API

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,52 @@
 <template>
    <div class="form">
-        <h3>This is simple Form</h3>
-        <h4>{{message}}</h4>
-        <slot/>
-        <h3>Employee Form</h3>
         <form>
-            <p>
-            <label>Name :</label>
-            <input type="text" name="name"  v-model.lazy="employee.name" placeholder="Enter your name">
-            </p>
-            <p>
-            <label>Email :</label>
-            <input type="email" name="email" v-model.lazy="employee.email" placeholder="Enter your name">
-            </p>
-            <p>
-            <label>Designation : </label>
-            <input type="text" name="desg" v-model="employee.desg" placeholder="Enter your Designation">
-            </p>
-            <p>
-              <label><input type="checkbox" value="developer" v-model="employee.posts"> Developer</label>
-              <label><input type="checkbox" value="business analyst" v-model="employee.posts">Business Analyst</label>
-              <label><input type="checkbox" value="admin" v-model="employee.posts"> Admin</label>
-              <label><input type="checkbox" value="manager" v-model="employee.posts"> Manager</label>
-            </p>
-            <p>Gender:
-              <label><input type="radio" id="male" value="male" v-model="employee.gender">Male </label>
-              <label><input type="radio" id="female" value="female" v-model="employee.gender">Female</label>
-            </p>
-            <p>
-              <select  v-model="employee.label">
-                <option v-for="label in employee.labels" :key="label.index">{{label}}</option>
-              </select>
-            </p>
+          <p>
+            <label>Title :</label>
+            <input type="text" name="title" v-model.lazy="post.title">
+          </p>
+           <p>
+              <textarea name="body" v-model.lazy="post.body" placeholder="description"></textarea>
+          </p> 
+          <p>
+            <button v-on:click.prevent="blogPost">Publish</button>
+          </p>
         </form>
 
         <div class="data-preview">
-          <h4>Name: {{employee.name}}</h4>
-          <h4>Email : {{employee.email}}</h4>
-          <h4>Designation : {{employee.desg}}</h4>
-          <h4>Posts:</h4>
-          <ul>
-            <li v-for="post in employee.posts" :key="post.index">
-              {{post}}
-            <li/>
-          </ul>
-          <h4>Gender: {{employee.gender}}</h4>
-          <h4>Label : {{employee.label}}</h4>
+          <h3>{{post.title}}</h3>
+          <p>{{post.body}}</p>
         </div>
     </div>
 </template>
 
 <script>
-import Students from './components/Students';
-import Employees from './components/Employees';
+
 
 export default {
   data () {
     return {
-      employee:{
-        name:"",
-        email:"",
-        desg:"",
-        posts:[],
-        gnder:"",
-        labels:["senior","junior","motivator"]
+      post:{
+        userId:'1',
+        id:'1',
+        title:'',
+        body:''
       }
     }
   },
-  components:{
-   
+  methods:{
+    blogPost(){
+      // Data section for Post Request
+       this.$http.post("https://jsonplaceholder.typicode.com/posts",{
+          title:this.post.title,
+          userId:1,
+          id:2,
+          body:this.post.body
+       }).then(function(data){
+        //  Response section
+          console.log(data);
+       });
+    }
   },  
 }
 </script>
